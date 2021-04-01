31d2bcf934cb296ef22bedb4e00958263fca7a7b
chaewoong0407 <an0304073@naver.com>
2021-04-01 12:02:12 +0900

    modify pagedata petition to movie

--- src/components/PetitionPage.js
+++ src/components/PetitionPage.js
@@ -1,69 +1,56 @@
-import React, { useState } from "react";
-import { paginate } from "../utils/paginate";
-import Pagenation from "./common/Pagenation";
+/***** ./components/MoviesPage.jsx *****/
 
-const PetitionPage = () => {
-  const getPeitions = () => {
-    const petitions = [
+import React, { useState } from "react";
+import Pagination from "./common/Pagenation";
+import { paginate } from "../utils/paginate";
+
+const MoviesPage = () => {
+  const getMovies = () => {
+    const movies = [
+      { id: 0, title: "기생충", genre: "블랙 코미디", release: "2019-05-30" },
+      { id: 1, title: "라이온 킹", genre: "애니메이션", release: "2019-07-17" },
       {
-        id: 20,
-        title: "전공동아리 시간을 좀 더 늘려주세요!",
-        agreed: 85,
-        end_at: "2020.11.09",
+        id: 2,
+        title: "날씨의 아이",
+        genre: "애니메이션",
+        release: "2019-10-31",
       },
+      { id: 3, title: "알라딘", genre: "판타지", release: "2019-05-23" },
+      { id: 4, title: "나랏말싸미", genre: "역사", release: "2019-07-24" },
+      { id: 5, title: "주전장", genre: "역사", release: "2019-07-25" },
       {
-        id: 19,
-        title: "점심시간에 운동장에 노래 틀어주세요..",
-        agreed: 40,
-        end_at: "2020.11.09",
+        id: 6,
+        title: "어벤져스: 엔드게임",
+        genre: "판타지",
+        release: "2019-04-24",
       },
-      {
-        id: 18,
-        title: "1학년 싱가포르 보내주세요 제발요",
-        agreed: 100,
-        end_at: "2020.11.08",
-      },
-      {
-        id: 17,
-        title: "2학년 자유여행 꼭 보내주세요",
-        agreed: 168,
-        end_at: "2020.11.07",
-      },
-      {
-        id: 16,
-        title: "전공캠프 횟수를 늘려주세요!!",
-        agreed: 20,
-        end_at: "2020.11.05",
-      },
-      {
-        id: 16,
-        title: "전공캠프 횟수를 늘려주세요!!",
-        agreed: 20,
-        end_at: "2020.11.05",
-      },
+      { id: 7, title: "봉오동 전투", genre: "역사", release: "2019-08-07" },
+      { id: 8, title: "김복동", genre: "역사", release: "2019-08-08" },
+      { id: 9, title: "코코", genre: "애니메이션", release: "2018-01-11" },
     ];
-    return petitions;
+    return movies;
   };
 
-  const [petitions, setPetitions] = useState({
-    data: getPeitions(),
-    pageSize: 3,
-    currentPage: 1,
+  const [movies, setMovies] = useState({
+    // 영화 정보를 담는 state
+    data: getMovies(), // 영화 정보
+    pageSize: 3, // 한 페이지에 보여줄 아이템(영화목록) 개수
+    currentPage: 1, // 현재 활성화된 페이지 위치
   });
 
   const handlePageChange = (page) => {
-    setPetitions({ ...petitions, currentPage: page });
+    setMovies({ ...movies, currentPage: page });
   };
 
-  const { data, pageSize, currentPage } = petitions;
-  const pagedPetitions = paginate(data, currentPage, pageSize);
+  const { data, pageSize, currentPage } = movies;
+  const pagedMovies = paginate(data, currentPage, pageSize); // 페이지 별로 아이템이 속한 배열을 얻어옴
 
-  const { length: count } = petitions.data;
-  if (count === 0) return <div>청원이 없습니다.</div>;
+  const { length: count } = movies.data;
+  if (count === 0) return <p>영화 정보가 없습니다.</p>;
 
   return (
     <>
-      <div>{count} 개의 영화 정보가 있습니다.</div>
+      <p>{count} 개의 영화 정보가 있습니다.</p>
 
       <table className="table">
         <thead>
@@ -75,20 +62,19 @@ const PetitionPage = () => {
           </tr>
         </thead>
         <tbody>
-          {petitions.data.map((petition) => (
-            <tr key={petition.id}>
-              <td>{petition.id}</td>
-              <td>{petition.title}</td>
-              <td>{petition.agreed}</td>
-              <td>{petition.end_at}</td>
+          {pagedMovies.map((movie) => (
+            <tr key={movie.id}>
+              <td>{movie.id}</td>
+              <td>{movie.title}</td>
+              <td>{movie.genre}</td>
+              <td>{movie.release}</td>
             </tr>
           ))}
         </tbody>
       </table>
-
-      <Pagenation
+      <Pagination
+        pageSize={pageSize}
         itemsCount={count}
-        pageSize={petitions.pageSize}
         currentPage={currentPage}
         onPageChange={handlePageChange}
       />
@@ -96,4 +82,4 @@ const PetitionPage = () => {
   );
 };
 
-export default PetitionPage;
+export default MoviesPage;
