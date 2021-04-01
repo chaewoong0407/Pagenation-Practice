fe13ff51afe4facf44a9a079f7c04b041d03eb6d
chaewoong0407 <an0304073@naver.com>
2021-04-01 10:27:24 +0900

    setting Prettier

--- src/components/PetitionPage.js
+++ src/components/PetitionPage.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const PetitionPage = () => {
-    return (
-        <div>
-            ss
-        </div>
-    )
-}
+  const getPeitions = () => {
+    const petitions = [
+      {
+        id: 20,
+        title: "전공동아리 시간을 좀 더 늘려주세요!",
+        agreed: 85,
+        end_at: "2020.11.09",
+      },
+      {
+        id: 19,
+        title: "점심시간에 운동장에 노래 틀어주세요..",
+        agreed: 40,
+        end_at: "2020.11.09",
+      },
+      {
+        id: 18,
+        title: "1학년 싱가포르 보내주세요 제발요",
+        agreed: 100,
+        end_at: "2020.11.08",
+      },
+      {
+        id: 17,
+        title: "2학년 자유여행 꼭 보내주세요",
+        agreed: 168,
+        end_at: "2020.11.07",
+      },
+      {
+        id: 16,
+        title: "전공캠프 횟수를 늘려주세요!!",
+        agreed: 20,
+        end_at: "2020.11.05",
+      },
+    ];
+  };
+  return <div>ss</div>;
+};
 
-export default PetitionPage
+export default PetitionPage;
